7395b2c888d281954bdf6ed58ef3871f1d93d6f9
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-04-27 16:27:22 -0700

    initial commit

---
+++ PA2_code/constants.py
@@ -0,0 +1,19 @@
+seed = 42
+
+""" Hyperparameters to use for training to roughly match 
+the numbers mentioned in the assignment description """
+batch_size = 16  # Number of independent sequences  we will process in parallel
+block_size = 32  # Maximum context length for predictions
+learning_rate = 1e-3  # Learning rate for the optimizer
+n_embd = 64  # Embedding dimension
+n_head = 2  # Number of attention heads
+n_layer = 4  # Number of transformer layers
+
+
+## classifier training hyperparameters. It is a simple 1 hidden layer feedforward network, with input 
+## size of 64, hidden size of 50 and output size of 3.
+
+n_input = 64  # Input size for the classifier, should match the embedding size of the transformer
+n_hidden = 100  # Hidden size for the classifier
+n_output = 3  # Output size for the classifier, we have 3 classes
+epochs_CLS = 15 # epochs for classifier training
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # 156_PA2
 Programming Assignment 2 for CSE 156: Statistical NLP
+
+The directory "PA2_code" contains the starter code.
+Feel free to ignore the starter code and implement from scratch.
+However, you should use the base settings for the hyperparameters, as specified in main.py.
+
+The directory "speechesdataset" contains the data to be used for this assignment.
