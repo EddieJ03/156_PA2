61d67dc6ccc521e87662327e94738e77cfed69ae
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-05-07 22:56:50 -0700

    removed comments

--- PA2_code/transformer.py
+++ PA2_code/transformer.py
@@ -19,29 +19,25 @@ class Head(nn.Module):
         # self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, attention_maps):
-        # input of size (batch, time-step, channels)
-        # output of size (batch, time-step, head size)
         B,T,C = x.shape
         
-        k = self.key(x)   # (B,T,hs)
-        q = self.query(x) # (B,T,hs)
-        # compute attention scores ("affinities")
-        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 # (B, T, hs) @ (B, hs, T) -> (B, T, T)
+        k = self.key(x)   
+        q = self.query(x) 
+        
+        wei = q @ k.transpose(-2,-1) * k.shape[-1]**-0.5 
         
         if self.decoding:
-            # print("mask??")
-            wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) # (B, T, T)
+            wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf')) 
         
-        wei = F.softmax(wei, dim=-1) # (B, T, T)
+        wei = F.softmax(wei, dim=-1) 
         
         # wei = self.dropout(wei)
         
         attention_maps.append(wei)
         
-        # perform the weighted aggregation of the values
-        v = self.value(x) # (B,T,hs)
+        v = self.value(x) 
         
-        out = wei @ v # (B, T, T) @ (B, T, hs) -> (B, T, hs)
+        out = wei @ v 
         
         return out
 
@@ -77,7 +73,6 @@ class Block(nn.Module):
     """ Transformer block: communication followed by computation """
 
     def __init__(self, n_embd, n_head=n_head, decoding=False):
-        # n_embd: embedding dimension, n_head: the number of heads we'd like
         super().__init__()
         head_size = n_embd // n_head
         self.sa: MultiHeadAttention = MultiHeadAttention(n_head, head_size, decoding)
@@ -96,8 +91,6 @@ class Classifier(nn.Module):
         self.fc1 = nn.Linear(input_size, hidden_size)  # First fully connected layer.
         self.fc2 = nn.Linear(hidden_size, n_output)  # Second fully connected layer, outputting two classes.
 
-    # Define the forward pass of the neural network.
-    # x: The input tensor.
     def forward(self, x):
         x = F.relu(self.fc1(x))  # Apply ReLU activation function after the first layer.
         x = self.fc2(x)  # Pass the result to the second layer.
@@ -108,27 +101,22 @@ class Classifier(nn.Module):
 class Encoder(nn.Module):
     def __init__(self, vocab_size):
         super().__init__()
-        # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.blocks = nn.ModuleList([Block(n_embd, n_head=n_head, decoding=False) for _ in range(n_layer)])
-        # self.ln_f = nn.LayerNorm(n_embd) # final layer norm
         self.classifier = Classifier(input_size=n_embd, hidden_size=n_hidden)
 
     def forward(self, idx):
         B, T = idx.shape
 
-        # idx and targets are both (B,T) tensor of integers
-        tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arange(T)) # (T,C)
-        x = tok_emb + pos_emb # (B,T,C)
+        tok_emb = self.token_embedding_table(idx) 
+        pos_emb = self.position_embedding_table(torch.arange(T)) 
+        x = tok_emb + pos_emb 
         
         attention_maps = []
         
         for block in self.blocks:
-           x = block(x, attention_maps) # (B,T,C)
-        
-        # x = self.ln_f(x) # (B,T,C)
+           x = block(x, attention_maps) 
         
         x = torch.mean(x, dim=1)
         
@@ -137,26 +125,24 @@ class Encoder(nn.Module):
 class Decoder(nn.Module):
     def __init__(self, vocab_size):
         super().__init__()
-        # each token directly reads off the logits for the next token from a lookup table
         self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
         self.position_embedding_table = nn.Embedding(block_size, n_embd)
         self.blocks = nn.ModuleList([Block(n_embd, n_head=n_head, decoding=True) for _ in range(n_layer)])
-        self.ln_f = nn.LayerNorm(n_embd) # final layer norm
+        self.ln_f = nn.LayerNorm(n_embd) 
         self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx):
         B, T = idx.shape
-
-        # idx and targets are both (B,T) tensor of integers
-        tok_emb = self.token_embedding_table(idx) # (B,T,C)
-        pos_emb = self.position_embedding_table(torch.arange(T)) # (T,C)
         
-        x = tok_emb + pos_emb # (B,T,C)
+        tok_emb = self.token_embedding_table(idx) 
+        pos_emb = self.position_embedding_table(torch.arange(T))
+        
+        x = tok_emb + pos_emb 
         
         attention_maps = []
         
         for block in self.blocks:
-           x = block(x, attention_maps) # (B,T,C)
+           x = block(x, attention_maps) 
            
-        x = self.ln_f(x) # (B,T,C)
-        return self.lm_head(x), attention_maps # (B,T,vocab_size)
+        x = self.ln_f(x) 
+        return self.lm_head(x), attention_maps 
