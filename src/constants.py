3d9c5e4d41b3be975b136757059381b16be1c1f8
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-06-21 23:22:31 -0700

    change block size to 32

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ seed = 42
 """ Hyperparameters to use for training to roughly match 
 the numbers mentioned in the assignment description """
 batch_size = 16  # Number of independent sequences  we will process in parallel
-block_size = 1024  # Maximum context length for predictions
+block_size = 32  # Maximum context length for predictions
 learning_rate = 1e-3  # Learning rate for the optimizer , OG: 1e-3
 n_embd = 64  # Embedding dimension
 n_head = 2 # Number of attention heads, OG = 2
